b5018b00c4346e8a1a8ee95474b9a61415360aa7
Mark Peters <mapeters101@earthlink.net>
2025-03-12 23:15:20 -0400

    Descriptions for exercise01.

---
+++ exercise01.py
@@ -0,0 +1,7 @@
+'''
+Question 1
+Your code uses a few functions in the math module here and there, but you also
+tend to use the sin, cos and tan functions and the pi constant very frequently.
+
+Write some import statements that you think would be helpful in this scenario.
+'''
