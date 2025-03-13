f3328cce84daee2280e53d9b544e045643e7d868
Mark Peters <mapeters101@earthlink.net>
2025-03-12 23:14:11 -0400

    Reformat descriptions for all exercises.

--- exercises_all.py
+++ exercises_all.py
@@ -1,13 +1,17 @@
 '''
 Question 1
-Your code uses a few functions in the math module here and there, but you also tend to use the sin, cos and tan functions and the pi constant very frequently.
+Your code uses a few functions in the math module here and there, but you also
+tend to use the sin, cos and tan functions and the pi constant very frequently.
 
 Write some import statements that you think would be helpful in this scenario.
 
 Question 2
 There is a library that we installed for our course called matplotlib.
 
-We can certainly import that library using its full name, but whenever we need to reach into that module we need to type out matplotlib.som_func - since we use this library quite often, typing out matplotlib every time gets tiring.
+We can certainly import that library using its full name, but whenever we need
+to reach into that module we need to type out matplotlib.som_func - since we
+use this library quite often, typing out matplotlib every time gets tiring.
 
-Write an import function that allows you to reference that module using the name mpl instead of the full name matplotlib.
+Write an import function that allows you to reference that module using the
+name mpl instead of the full name matplotlib.
 '''
