3c002217a7e82c51047f7f24c95e6adf3c611841
Mark Peters <mapeters101@earthlink.net>
2025-03-12 23:16:23 -0400

    Descriptions for exercise02.

---
+++ exercise02.py
@@ -0,0 +1,11 @@
+'''
+Question 2
+There is a library that we installed for our course called matplotlib.
+
+We can certainly import that library using its full name, but whenever we need
+to reach into that module we need to type out matplotlib.som_func - since we
+use this library quite often, typing out matplotlib every time gets tiring.
+
+Write an import function that allows you to reference that module using the
+name mpl instead of the full name matplotlib.
+'''
